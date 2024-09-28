048553cb4382df0a58ee0d88c20dfc99f542681c
“bheemrajkumawat” <bheemrajkumawat127@gmail.com>
2024-09-28 16:45:08 +0530

    added function

--- system.js
+++ system.js
@@ -1,13 +1,13 @@
-function New() {
-  const name = "bheem";
-  if (true) {
-    const name = "raj";
-    console.log(name);
-  }
-  console.log(name);
-}
+// function New() {
+//   const name = "bheem";
+//   if (true) {
+//     const name = "raj";
+//     console.log(name);
+//   }
+//   console.log(name);
+// }
 
-New();
+// New();
 
 //  block scoped let and const
 // function number(){
@@ -39,27 +39,98 @@ New();
 // }
 // document();
 
-
-
 // template-literal-string
-let Name="bheemraj"
-let lastName="kumawat"
-console.log("my name is "+Name + lastName );
+// let Name="bheemraj"
+// let lastName="kumawat"
+// console.log("my name is "+Name + lastName );
 
-console.log(`my name is ${Name}${lastName}`)
+// console.log(`my name is ${Name}${lastName}`)
+
+// let a=20;
+// let b=30;
+// console.log(a +"="+(a+b)+" "+ b+"="+(a-b) );
+
+// console.log(`${a}=${a+b} ${b}=${a-b}`)
+
+// // template-literal( template string)method
+
+// let name="bheemraj"
+// let last_name="kumawat"
+// console.log(`${name}`.startsWith("b"))
+// console.log(`${name}`.endsWith("j"))
+// console.log(`${name} `.repeat(100))
+
+// Array Destructuring in ES6
+
+// let arr=["js","php","python","c","java"]
+//  console.log(arr.length);
+//  let arr1=arr[0]
+//  let arr2=arr[1]
+//  let arr3=arr[2]
+//  let arr4=arr[3]
+//  let arr5=arr[4]
+//  console.log(`my fav language ${arr1} and last fav language${arr2}`)
+
+//  let [arr1,arr2,arr3,arr4,arr5]=arr
+//  console.log(`my fav language ${arr1} and last fav language ${arr3}`)
+
+//  console.log(arr1.toUpperCase())
+//  console.log(arr1.toLowerCase())
+//  console.log(arr1.replace("js","javascript"))
+//  console.log(arr1.concat(arr3))
+//  console.log(arr1.repeat(100))
+
+//javascript program swap two number
+
+// let i=30;
+// let j=40;
+
+// let temp=i
+// // console.log(temp)
+// i=j;
+// // console.log(i)
+// j=temp
+// // console.log(j)
+// console.log(`i=${i} j=${j}`)
+
+// [i,j]=[j,i];// using two var.
+// console.log(i,j)
 
 
-let a=20;
-let b=30;
-console.log(a +"="+(a+b)+" "+ b+"="+(a-b) );
+//Object Destructuring in ES6
+const obj = {
+  Name: "bheemraj",
+  lastName: "kumawat",
+  age: 21,
+};
+let {Name,lastName,age}=obj
 
-console.log(`${a}=${a+b} ${b}=${a-b}`)
+console.log(`my name is ${Name}${lastName} my age ${age} year`)
 
-// template-literal( template string)method
- 
-let name="bheemraj"
-let last_name="kumawat"
-console.log(`${name}`.startsWith("b"))
-console.log(`${name}`.endsWith("j"))
-console.log(`${name} `.repeat(100))
+
+
+//fat arrow function in javascript
+
+const num=function (){
+          a=30;
+          b=20;
+          return a+b;
+}
+console.log(num())
+
+//es6
+
+let i=40;
+let j=50;
+const sum =()=>{
+return i+j
+          
+}
+console.log(sum())
+
+const num1=(q,w)=>{return  q+w}
+console.log(num1(40,90));
+
+// ES6 Default Parameters 
+
 
